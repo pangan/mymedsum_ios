caa95c85960d987a085fb634151fe0190eba54c7
Amir <amir@lifesum.com>
2020-05-08 20:04:46 +0200

    Initial Commit

---
+++ MyMedSum/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyMedSum
+//
+//  Created by Amir Mofakhar on 2020-05-08.
+//  Copyright © 2020 Amir Mofakhar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
